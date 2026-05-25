b8ea2054c0476c4a2deafc14c441cd662292dfde
Diego Alves <diegohalves.dev@gmail.com>
2026-05-25 19:29:42 -0400

    Exercicio 1

--- src/main/java/br/com/diegoalves/Main.java
+++ src/main/java/br/com/diegoalves/Main.java
@@ -2,6 +2,19 @@ package br.com.diegoalves;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        /*
+        * Exercicio 1
+            Crie uma classe com main que declare uma variavel int nota = 8 e
+            * uma variavel Integer notaObj =
+            nota. Imprima ambas e imprima o tipo da notaObj
+            * usando getClass().getSimpleName().
+        * */
+
+        int nota = 8;
+        Integer notaObj = nota;
+
+        System.out.println("Nota: " + nota);
+        System.out.println("NotaObj: " + notaObj);
+        System.out.println("O tipo é " + notaObj.getClass().getSimpleName());
     }
 }
