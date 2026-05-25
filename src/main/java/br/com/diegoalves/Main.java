1e8c165ada19ba8c3661e54545f932c0cebfb619
Diego Alves <diegohalves.dev@gmail.com>
2026-05-25 19:32:13 -0400

    Exercício 2

--- src/main/java/br/com/diegoalves/Main.java
+++ src/main/java/br/com/diegoalves/Main.java
@@ -3,18 +3,12 @@ package br.com.diegoalves;
 public class Main {
     public static void main(String[] args) {
         /*
-        * Exercicio 1
-            Crie uma classe com main que declare uma variavel int nota = 8 e
-            * uma variavel Integer notaObj =
-            nota. Imprima ambas e imprima o tipo da notaObj
-            * usando getClass().getSimpleName().
+        * Exercicio 2
+            Declare Integer x = null. Tente atribuir x a uma variavel int.
+            * O que acontece ao rodar? Explique
         * */
 
-        int nota = 8;
-        Integer notaObj = nota;
-
-        System.out.println("Nota: " + nota);
-        System.out.println("NotaObj: " + notaObj);
-        System.out.println("O tipo é " + notaObj.getClass().getSimpleName());
+       Integer x = null;
+       int a = x;
     }
 }
