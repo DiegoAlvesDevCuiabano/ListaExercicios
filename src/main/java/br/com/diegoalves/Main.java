9d902c2544113a4cc12061a12b21b941dd27195b
Diego Alves <diegohalves.dev@gmail.com>
2026-05-25 19:36:42 -0400

    Exercício 3

--- src/main/java/br/com/diegoalves/Main.java
+++ src/main/java/br/com/diegoalves/Main.java
@@ -3,12 +3,20 @@ package br.com.diegoalves;
 public class Main {
     public static void main(String[] args) {
         /*
-        * Exercicio 2
-            Declare Integer x = null. Tente atribuir x a uma variavel int.
-            * O que acontece ao rodar? Explique
+        Exercicio 3
+            Declare dois Integer com valor 127 e compare com ==.
+            Depois declare dois Integer com valor 200 e
+            compare com == e com .equals(). Explique a diferenca.
         * */
 
-       Integer x = null;
-       int a = x;
+        Integer a = 127;
+        Integer b = 127;
+
+        System.out.println(a == b);
+        Integer c = 200;
+        Integer d = 200;
+        System.out.println("=============Pulando linha============");
+        System.out.println(c == d);
+        System.out.println(c.equals(d));
     }
 }
